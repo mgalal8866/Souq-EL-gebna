d49c6952dc1f0a8ce629910a4b271783d1beae58
mgalal8866 <mgalal8866@gmail.com>
2023-09-21 12:56:08 +0300

    modified:   routes/api_v1.php

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -10,10 +10,16 @@ use App\Http\Controllers\Api\V1\SliderController;
 
 Route::post('login', [UserController::class, 'login']);
 Route::post('register', [UserController::class, 'register']);
+
 Route::get('/sendotp/{phone?}',   [UserController::class, 'sendotp']);
 Route::post('/verificationcode',   [UserController::class, 'verificationcode']);
+################################### Forgot Password ######################################
+################################### Forgot Password ######################################
 Route::get('/check/phone/{phone?}',   [UserController::class, 'checkphone']);
 Route::post('/check/answer',   [UserController::class, 'checkanswer']);
+################################### Forgot Password ######################################
+################################### Forgot Password ######################################
+
 
 Route::get('/city',   [UserController::class, 'city']);
 Route::get('/region/{id?}',   [UserController::class, 'region']);
