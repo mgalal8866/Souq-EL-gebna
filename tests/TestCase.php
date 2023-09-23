9b7848d7e2f22f0a78cdb355cc288c8cfc5f6b76
mgalal8866 <mgalal8866@gmail.com>
2023-09-23 04:09:09 +0300

    modified:   tests/TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,26 +2,9 @@
 
 namespace Tests;
 
-use App\Livewire\Dashboard\Brands;
-use Livewire\Livewire;
-use Illuminate\Support\Facades\Facade;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-      // ...
-
-        $this->registerLivewireComponents();
-    }
-
-    protected function registerLivewireComponents()
-    {
-        Livewire::component('Brands', Brands::class);
-    }
 }
