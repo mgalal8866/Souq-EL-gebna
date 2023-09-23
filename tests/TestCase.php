c11a782c4cc0b4b29625ca1167adb048626f6bed
mgalal8866 <mgalal8866@gmail.com>
2023-09-23 03:41:23 +0300

    modified:   tests/TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,60 @@
 
 namespace Tests;
 
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    public function setUp(): void
+    {
+
+        $this->afterApplicationCreated(function () {
+            $this->makeACleanSlate();
+        });
+
+        $this->beforeApplicationDestroyed(function () {
+            $this->makeACleanSlate();
+        });
+
+        parent::setUp();
+        Facade::setFacadeApplication(app());
+    }
+
+    public function makeACleanSlate()
+    {
+        Artisan::call('view:clear');
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            FlatformServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('view.paths', [
+            __DIR__.'/../views',
+            resource_path('views'),
+        ]);
+
+        $app['config']->set('flatform', require  __DIR__ . '\..\config\flatform.php');
+        $app['config']->set('session.driver', 'file');
+        $app['config']->set('app.key', 'base64:Hupx3yAySikrM2/edkZQNQHslgDWYfiBfCuSThJ5SK8=');
+        $app['config']->set('flatform.test', 1);
+    }
+
+    protected function resolveApplicationHttpKernel($app)
+    {
+        $app->singleton('Illuminate\Contracts\Http\Kernel', 'Tests\HttpKernel');
+    }
+
+    public function test_test_envirement()
+    {
+        $this->assertNotNull(app('flatform'));
+    }
 }
