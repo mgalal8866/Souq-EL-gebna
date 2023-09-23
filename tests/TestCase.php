b6365a6005535a23657403a5a36cac46992610ea
mgalal8866 <mgalal8866@gmail.com>
2023-09-23 03:43:36 +0300

    modified:   tests/TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use Livewire\Livewire;
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
@@ -9,53 +10,17 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-    public function setUp(): void
+    protected function setUp(): void
     {
-
-        $this->afterApplicationCreated(function () {
-            $this->makeACleanSlate();
-        });
-
-        $this->beforeApplicationDestroyed(function () {
-            $this->makeACleanSlate();
-        });
-
         parent::setUp();
-        Facade::setFacadeApplication(app());
+
+      // ...
+
+        $this->registerLivewireComponents();
     }
 
-    public function makeACleanSlate()
+    protected function registerLivewireComponents()
     {
-        Artisan::call('view:clear');
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [
-            FlatformServiceProvider::class,
-        ];
-    }
-
-    protected function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('view.paths', [
-            __DIR__.'/../views',
-            resource_path('views'),
-        ]);
-
-        $app['config']->set('flatform', require  __DIR__ . '\..\config\flatform.php');
-        $app['config']->set('session.driver', 'file');
-        $app['config']->set('app.key', 'base64:Hupx3yAySikrM2/edkZQNQHslgDWYfiBfCuSThJ5SK8=');
-        $app['config']->set('flatform.test', 1);
-    }
-
-    protected function resolveApplicationHttpKernel($app)
-    {
-        $app->singleton('Illuminate\Contracts\Http\Kernel', 'Tests\HttpKernel');
-    }
-
-    public function test_test_envirement()
-    {
-        $this->assertNotNull(app('flatform'));
+        Livewire::component('comment', Comment::class);
     }
 }
