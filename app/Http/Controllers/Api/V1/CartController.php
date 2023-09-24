ce0468197d0657ad75edbdb7646d34ed05d88de2
mgalal8866 <mgalal8866@gmail.com>
2023-09-24 16:12:02 +0300

    modified:   app/Http/Controllers/Api/V1/CartController.php 	modified:   app/Providers/RepositoryServiceProvider.php

--- app/Http/Controllers/Api/V1/CartController.php
+++ app/Http/Controllers/Api/V1/CartController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Repositoryinterface\CartRepositoryinterface;
-use SebastianBergmann\CodeCoverage\Report\Xml\Report;
+
 
 class CartController extends Controller
 {
--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -3,17 +3,18 @@
 namespace App\Providers;
 
 use App\Repository\DBCartRepository;
-use Illuminate\Support\ServiceProvider;
 use App\Repository\DBUserRepository;
-use App\Repository\DBSlideRepository;
-use App\Repository\DBCategoryRepository;
 use App\Repository\DBItemsRepository;
 use App\Repository\DBOrderRepository;
+use App\Repository\DBSlideRepository;
+use Illuminate\Support\ServiceProvider;
+use App\Repository\DBCategoryRepository;
+use App\Repositoryinterface\CartRepositoryinterface;
 use App\Repositoryinterface\UserRepositoryinterface;
-use App\Repositoryinterface\Sliderepositoryinterface;
-use App\Repositoryinterface\CategoryRepositoryinterface;
 use App\Repositoryinterface\ItemsRepositoryinterface;
 use App\Repositoryinterface\OrderRepositoryinterface;
+use App\Repositoryinterface\Sliderepositoryinterface;
+use App\Repositoryinterface\CategoryRepositoryinterface;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
@@ -22,15 +23,14 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-       
+
         $this->app->bind(OrderRepositoryinterface::class, DBOrderRepository::class);
-        $this->app->bind(CategoryRepositoryinterface::class, DBCartRepository::class);
+        $this->app->bind(CartRepositoryinterface::class, DBCartRepository::class);
         $this->app->bind(ItemsRepositoryinterface::class, DBItemsRepository::class);
         $this->app->bind(UserRepositoryinterface::class, DBUserRepository::class);
         $this->app->bind(CategoryRepositoryinterface::class, DBCategoryRepository::class);
         $this->app->bind(Sliderepositoryinterface::class, DBSlideRepository::class);
         $this->app->bind(UserRepositoryinterface::class, DBUserRepository::class);
-
     }
     public function boot(): void
     {
