d97125e7d1948654bf028984725d0a7ac49c3f8f
mgalal8866 <mgalal8866@gmail.com>
2023-09-28 00:11:13 +0300

    modified:   app/Repository/DBUserRepository.php 	modified:   routes/api_v1.php

--- app/Repository/DBUserRepository.php
+++ app/Repository/DBUserRepository.php
@@ -75,6 +75,10 @@ class DBUserRepository implements UserRepositoryinterface
         if ($token == null) {
             return Resp(null, 'User Not found', 404, false);
         }
+        if (auth('api')->user()->active == 0) {
+            return Resp(null, 'تم ايقاف حسابك ', 404, false);
+        }
+
         $user =  auth('api')->user();
         $user->token = $token;
         $data =  new LoginUserResource($user);
