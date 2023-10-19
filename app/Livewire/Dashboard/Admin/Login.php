e3824e1fdc72ffa6c4c157afbd8eba4e7af80d92
mgalal8866 <mgalal8866@gmail.com>
2023-10-20 02:07:56 +0300

    new file:   app/Livewire/Dashboard/Admin/Login.php 	new file:   resources/views/dashboard/admin/login.blade.php

---
+++ app/Livewire/Dashboard/Admin/Login.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Dashboard\Admin;
+
+use Livewire\Component;
+
+class Login extends Component
+{
+    public function render()
+    {
+        return view('dashboard.admin.login');
+    }
+}
