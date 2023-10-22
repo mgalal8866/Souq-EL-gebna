9b6782e9e0045edcd55b7043983566e942bc4239
mgalal8866 <mgalal8866@gmail.com>
2023-10-22 21:12:46 +0300

    modified:   app/Repository/DBOrderRepository.php

--- app/Repository/DBOrderRepository.php
+++ app/Repository/DBOrderRepository.php
@@ -103,7 +103,7 @@ class DBOrderRepository implements OrderRepositoryinterface
     public function change_statu($request)
     {
         $suborder = SubOrder::find($request['suborder_id']);
-        dd($suborder);
+       
         $suborder->update(['sub_statu_delivery' => $request['statu']]);
         switch ($request['statu']) {
             case '1':
