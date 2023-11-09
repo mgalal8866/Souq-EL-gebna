17268cd19a2c91a68b7506cbd613188ed9d2b130
mgalal8866 <mgalal8866@gmail.com>
2023-11-10 00:29:02 +0200

    modified:   resources/views/layouts/dashboard/head.blade.php

--- resources/views/layouts/dashboard/head.blade.php
+++ resources/views/layouts/dashboard/head.blade.php
@@ -2,6 +2,8 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>@yield('title','ثلاجة جبنة')</title>
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('asset/images/favicon.ico') }}">
 
